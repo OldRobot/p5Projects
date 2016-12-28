43e5a70da98acda4b9b9089084c73648041d2361
Paul Smith <Paul Smith>
2016-12-28 19:06:58 +0000

    Modified startup file

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,45 +1,16 @@
-var axiom = "A";
-var sentence = axiom;
+//Paul Smith
+//Base file
 
-var rule1 = {
-  a: "A",
-  b: "AB"
-}
-
-var rule2 = {
-  a: "B",
-  b: "A"
-}
-
-function generate(){
-  var nextSentence = "";
-  for(var i=0;i<sentence.length;i++){
-    var current = sentence.charAt(i);
-    if (current == rule1.a){
-      nextSentence += rule1.b;
-    }else if (current == rule2.a){
-      nextSentence += rule2.b;
-    }else{
-      nextSentence += current;
-     }
-    }
-    sentence = nextSentence;
-    createP(sentence);
-}
 
 function setup() {
-//createCanvas(400, 400);
+createCanvas(400, 400);
 //fr = createP('');
 //fr.html(floor(frameRate())
-noCanvas();
-createP(axiom);
-var button = createButton("generate");
-button.mousePressed(generate);
 
 }
 
 function draw() {
-  //background(50);
+background(50);
 
-noLoop();
+
 }
