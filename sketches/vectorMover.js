766aed70ecfdf7bfb9c7048d9207e4d2ea582ab8
Paul Smith <Paul Smith>
2016-12-29 09:42:25 +0000

    adding forces

--- sketches/vectorMover.js
+++ sketches/vectorMover.js
@@ -10,24 +10,65 @@ function setup(){
 }
 
 function draw(){
+background(50);
+gravity = createVector(0,0.1);
+m.force(gravity);
+m.update();
+m.bounceEdges();
 
 m.display();
-m.update();
+m.acceleration.mult(0);//reset acceleration each frame
 }
 
 
 function mover(){
 this.location = createVector(random(width),random(height));
-this.velocity = createVector(random(-2,2),random(-2,2));
+this.velocity = createVector(0,0); //initial
+this.acceleration = createVector(0.2,0);
+//this.velocity = createVector(random(-4,4),random(-4,4)); //initial velocity
+this.r = 20; //radius
 
 this.update = function(){
+  this.velocity.add(this.acceleration);
   this.location.add(this.velocity);
+}
+
+this.force = function(f){
+this.acceleration.add(f);
 }
 
 this.display = function(){
   noStroke();
   fill(200);
-  ellipse(this.location.x,this.location.y,10,10);
+  ellipse(this.location.x,this.location.y,this.r,this.r);
 }
+
+this.wrapEdges = function(){
+  if (this.location.x>width){
+    this.location.x=0;
+  }else if(this.location.x<0) {
+    this.location.x = width;
+  }
+
+  if (this.location.y>height){
+    this.location.y=0;
+  }else if(this.location.y<0) {
+    this.location.y = height;
+  }
+
+}
+
+this.bounceEdges = function(){
+  if (this.location.x>width || this.location.x<0){
+    this.velocity.x = this.velocity.x * -1;
+  }
+
+  if (this.location.y>height || this.location.y<0){
+    this.velocity.y= this.velocity.y *-1;
+  }
+
+
+}
+
 
 }
