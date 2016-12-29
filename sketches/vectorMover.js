c3e57fc8a3ab476d82918bb08bbcd6b7fc9665af
Paul Smith <Paul Smith>
2016-12-29 18:38:16 +0000

    updated mover class to accept initial variables

--- sketches/vectorMover.js
+++ sketches/vectorMover.js
@@ -1,39 +1,50 @@
 //Paul Smith
 //A base example of using vectors for motion
-var m; //the mover
+var m = []; //the mover
 
 function setup(){
   createCanvas(400,400);
   background(50);
-  m = new mover();
+  for (var i=0;i<10;i++){
+      m[i] = new mover(random(width),random(height),random(5,40));
+  }
+
 
 }
 
 function draw(){
 background(50);
-gravity = createVector(0,0.1);
-m.force(gravity);
-m.update();
-m.bounceEdges();
+for (var i=0;i<10;i++){
 
-m.display();
-m.acceleration.mult(0);//reset acceleration each frame
+gravity = createVector(0,0.1);
+m[i].force(gravity);
+wind = createVector(0.02,0);
+m[i].force(wind);
+
+m[i].bounceEdges();
+m[i].update();
+m[i].display();
+}
 }
 
 
-function mover(){
-this.location = createVector(random(width),random(height));
+function mover(x,y,r){
+this.location = createVector(x,y);
 this.velocity = createVector(0,0); //initial
-this.acceleration = createVector(0.2,0);
+this.acceleration = createVector(0,0);
 //this.velocity = createVector(random(-4,4),random(-4,4)); //initial velocity
-this.r = 20; //radius
+this.r = r; //radius
+this.mass = sqrt(r);
 
 this.update = function(){
   this.velocity.add(this.acceleration);
   this.location.add(this.velocity);
+  this.acceleration.mult(0);//reset acceleration each frame
 }
 
-this.force = function(f){
+this.force = function(force){
+f= force.copy();
+f = f.div(this.mass);
 this.acceleration.add(f);
 }
 
