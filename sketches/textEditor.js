b7c12804f37d1db307d4f70e86566d072736caed
Paul Smith <Paul Smith>
2017-01-05 20:51:44 +0000

    text editor start

---
+++ sketches/textEditor.js
@@ -0,0 +1,52 @@
+
+//var myFont;
+
+//function preload(){
+//courier
+  //SmyFont = loadFont("Inconsolata-Regular.ttf");
+//}
+var c;
+
+function setup() {
+createCanvas(400, 400);
+//fr = createP('');
+//fr.html(floor(frameRate())
+c = new caret();
+}
+
+function caret(){
+  this.x=12*17;
+  this.line=18;
+  this.timer=0;
+  this.tInterval=80;
+
+this.draw = function(){
+  fill(255);
+    noStroke();
+  if(this.timer<this.tInterval/2){
+    fill(55);
+
+  }
+  rect(this.x+2,this.line,8,14);
+
+
+this.timer++;
+if(this.timer>this.tInterval){
+  this.timer=0;
+}
+}
+}
+
+function draw() {
+textSize(20);
+background(50);
+fill(255);
+noStroke();
+textFont("Courier");
+text("Testing the fonts",0,30);
+stroke(255);
+  // for (var i=0;i<200;i+=12){
+  //   line(i,0,i,200);
+  // }
+c.draw();
+}
